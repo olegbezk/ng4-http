c4fbf3b406420328886576bc8f6ae4d6240519fd
olegbezkorovanynyi <oleg.bezkorovaynyi@gmail.co>
2017-06-25 13:27:51 +0300

    setup firebase and sending post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ServerService } from './server.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,6 +20,16 @@ export class AppComponent {
       id: this.generateId()
     }
   ];
+
+  constructor(private serverService: ServerService) {}
+
+  onSave() {
+    this.serverService.storeServers(this.servers).subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+    );
+  }
+  
   onAddServer(name: string) {
     this.servers.push({
       name: name,
@@ -25,6 +37,7 @@ export class AppComponent {
       id: this.generateId()
     });
   }
+  
   private generateId() {
     return Math.round(Math.random() * 10000);
   }
